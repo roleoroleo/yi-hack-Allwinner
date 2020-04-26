e49df64902f06928d35c08abac114991e4d85b72
roleo <roleo@iperbole.bologna.it>
2020-04-26 12:27:52 +0200

    Add script to setup the environment

---
+++ src/static/static/home/yi-hack/script/env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/lib:/home/yi-hack/lib:/tmp/sd/yi-hack/lib
+export PATH=$PATH:/home/base/tools:/home/yi-hack/bin:/home/yi-hack/sbin:/home/yi-hack/usr/bin:/home/yi-hack/usr/sbin:/tmp/sd/yi-hack/bin:/tmp/sd/yi-hack/sbin
+
+if [ -f $YI_HACK_PREFIX/etc/TZ ]; then
+    export TZ=$(get_config TIMEZONE)
+fi
