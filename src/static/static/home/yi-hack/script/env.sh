cbd052654be4ea25789d79d7c34ad15fe79ae5f3
roleo <roleo@iperbole.bologna.it>
2020-05-02 19:47:31 +0200

    Fix bug in env.sh

--- src/static/static/home/yi-hack/script/env.sh
+++ src/static/static/home/yi-hack/script/env.sh
@@ -3,6 +3,12 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/lib:/home/yi-hack/lib:/tmp/sd/yi-hack/lib
 export PATH=$PATH:/home/base/tools:/home/yi-hack/bin:/home/yi-hack/sbin:/home/yi-hack/usr/bin:/home/yi-hack/usr/sbin:/tmp/sd/yi-hack/bin:/tmp/sd/yi-hack/sbin
 
+get_config()
+{
+    key=$1
+    grep -w $1 $YI_HACK_PREFIX/$CONF_FILE | cut -d "=" -f2
+}
+
 TZ_CONF=$(get_config TIMEZONE)
 
 if [ -z "$TZ_CONF" ]; then
