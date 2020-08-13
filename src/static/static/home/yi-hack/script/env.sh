a3161cd70e0a2217c38f492106de6709788d22cc
roleo <roleo@iperbole.bologna.it>
2020-08-13 22:09:41 +0200

    Fix env.sh

--- src/static/static/home/yi-hack/script/env.sh
+++ src/static/static/home/yi-hack/script/env.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+CONF_FILE="etc/system.conf"
+
+YI_HACK_PREFIX="/home/yi-hack"
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/lib:/home/yi-hack/lib:/tmp/sd/yi-hack/lib
 export PATH=$PATH:/home/base/tools:/home/yi-hack/bin:/home/yi-hack/sbin:/home/yi-hack/usr/bin:/home/yi-hack/usr/sbin:/tmp/sd/yi-hack/bin:/tmp/sd/yi-hack/sbin
 
@@ -11,6 +15,6 @@ get_config()
 
 TZ_CONF=$(get_config TIMEZONE)
 
-if [ -z "$TZ_CONF" ]; then
+if [ ! -z "$TZ_CONF" ]; then
     export TZ=$TZ_CONF
 fi
