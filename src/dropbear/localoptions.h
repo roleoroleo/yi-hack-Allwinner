976e855ba6c5cee9e4228aa5b1717ef10215c7b8
roleo <roleo@iperbole.bologna.it>
2020-08-19 16:03:33 +0200

    Fix wrong path of dbclient

--- src/dropbear/localoptions.h
+++ src/dropbear/localoptions.h
@@ -5,7 +5,7 @@
 #define RSA_PRIV_FILENAME "/home/yi-hack/etc/dropbear/dropbear_rsa_host_key"
 #define ECDSA_PRIV_FILENAME "/home/yi-hack/etc/dropbear/dropbear_ecdsa_host_key"
 
-#define DROPBEAR_PATH_SSH_PROGRAM "/usr/yi-hack/bin/dbclient"
+#define DROPBEAR_PATH_SSH_PROGRAM "/home/yi-hack/bin/dbclient"
 
 #define DEFAULT_PATH "/usr/bin:/usr/sbin:/bin:/sbin:/home/base/tools:/home/app/localbin:/home/base:/home/base/tools:/home/yi-hack/bin:/home/yi-hack/sbin:/home/yi-hack/usr/bin:/home/yi-hack/usr/sbin:/tmp/sd/yi-hack/bin:/tmp/sd/yi-hack/sbin"
 
