0e57551bd28bb566cc9b43692ac5cdea7f28e0db
roleo <roleo@iperbole.bologna.it>
2023-06-22 17:53:54 +0200

    Add hacked mode to unbrick procedure and add rootfs

--- unbrick/build.sh
+++ unbrick/build.sh
@@ -1,10 +1,75 @@
 #!/bin/bash
 
+update_init()
+{
+    ### Update /home/init.sh with a more friendly one
+    echo "### Updating $DIR/home/init.sh"
+    cp $DIR/home/app/init.sh $DIR/home/app/tmp_init.sh
+
+    echo "### Enabling telnet"
+    echo "# Running telnetd" >> $DIR/home/app/tmp_init.sh
+    echo "/usr/sbin/telnetd &" >> $DIR/home/app/tmp_init.sh
+
+    echo "### Enabling yi-hack script"
+    echo '/home/yi-hack/script/system.sh' >> $DIR/home/app/tmp_init.sh
+
+    ### Disable Yi junk in init.sh
+    echo "### Disabling Yi Junk"
+    sed -i 's/^.\/mp4record/#.\/mp4record/g' $DIR/home/app/tmp_init.sh
+    sed -i 's/^.\/cloud/#.\/cloud/g' $DIR/home/app/tmp_init.sh
+    sed -i 's/^.\/p2p_tnp/#.\/p2p_tnp/g' $DIR/home/app/tmp_init.sh
+    sed -i 's/^.\/oss/#.\/oss/g' $DIR/home/app/tmp_init.sh
+    sed -i 's/^.\/rtmp/#.\/rtmp/g' $DIR/home/app/tmp_init.sh
+    sed -i 's/^.\/watch_process/#.\/watch_process/g' $DIR/home/app/tmp_init.sh
+    sed -i 's/^.\/rmm/#.\/rmm/g' $DIR/home/app/tmp_init.sh
+    sed -i 's/^sleep 2/#sleep 2/g' $DIR/home/app/tmp_init.sh
+
+    cp -f $DIR/home/app/tmp_init.sh $DIR/home/app/init.sh
+    rm $DIR/home/app/tmp_init.sh
+    echo $RANDOM | md5sum | cut -d ' ' -f 1 > $DIR/home/.random
+
+    ### Replace /home/base/tools/extpkg.sh with a more friendly one
+    echo "### Updating $DIR/home/base/tools/extpkg.sh"
+    FILE="home/base/tools/extpkg.sh"
+    if [ -f $DIR/new$FILE ]; then
+        cp -r $DIR/new$FILE $DIR/$FILE
+    fi
+
+    ### Replace /home/app/script/wifidhcp.sh with a more friendly one
+    echo "### Updating $DIR/home/app/script/wifidhcp.sh"
+    FILE="home/app/script/wifidhcp.sh"
+    if [ -f $DIR/new$FILE ]; then
+        cp -r $DIR/new$FILE $DIR/$FILE
+    fi
+}
+
+TYPE=$1
+if [ "$TYPE" != "factory" ] && [ "$TYPE" != "hacked" ]; then
+    echo "Usage: $0 type"
+    echo -e "\twhere type = \"factory\" or \"hacked\""
+    exit
+fi
+
 for DIR in * ; do
     if [ -d $DIR ]; then
         FILENAME=`cat $DIR/filename`
+        if [ -f $DIR/mtdblock2.bin ]; then
+            mkimage -A arm -O linux -T filesystem -C none -a 0x0 -e 0x0 -n "xiaoyi-rootfs" -d $DIR/mtdblock2.bin $DIR/rootfs_$FILENAME
+            gzip $DIR/rootfs_$FILENAME
+        fi
         if [ -f $DIR/mtdblock3.bin ]; then
-            cp $DIR/mtdblock3.bin $DIR/mtdblock3_hacked.bin
+            if [ "$TYPE" == "hacked" ]; then
+                mkdir -p $DIR/mnt
+                ./mount.jffs2 $DIR/mtdblock3.bin $DIR/mnt 4
+                cp -r $DIR/mnt $DIR/home
+                umount $DIR/mnt
+                rm -rf $DIR/mnt
+                update_init
+                ./create.jffs2 0x00b80000 $DIR/home $DIR/mtdblock3_hacked.bin
+                rm -rf $DIR/home
+            else
+                cp $DIR/mtdblock3.bin $DIR/mtdblock3_hacked.bin
+            fi
             mkimage -A arm -O linux -T filesystem -C none -a 0x0 -e 0x0 -n "xiaoyi-home" -d $DIR/mtdblock3_hacked.bin $DIR/home_$FILENAME
             rm -f $DIR/mtdblock3_hacked.bin
             gzip $DIR/home_$FILENAME
---
+++ unbrick/y25ga/newhome/base/tools/extpkg.sh
@@ -0,0 +1,106 @@
+echo "------------------------------------home base tools extpkg.sh enter------------------------------------"
+cd /tmp/update
+cp -rf $1 /tmp/update/home_v429m
+
+# hack
+# test if bz2 file and jump directly to decompression function
+
+dd if=home_v429m of=header bs=2 count=1
+HEADER=`hexdump -n 2 -x header | grep 0000000 | awk '{print $2}'`
+if [ "$HEADER" == "5a42" ]; then
+    SWAP=`cat /proc/meminfo | grep SwapTotal | awk '{print $2}'`
+    if [[ "$SWAP" == "0" ]]; then
+        SD_PRESENT=$(mount | grep mmc | grep -c ^)
+        if [[ $SD_PRESENT -eq 1 ]] && [[ -f /home/yi-hack/sbin/mkswap ]]; then
+            rm -f /tmp/sd/swapfile_update
+            dd if=/dev/zero of=/tmp/sd/swapfile_update bs=1M count=64
+            chmod 0600 /tmp/sd/swapfile_update
+            mkswap /tmp/sd/swapfile_update
+            swapon /tmp/sd/swapfile_update
+        fi
+    fi
+    mv home_v429m home4
+else
+    cp /home/base/tools/rsa_pub_dec /home/base/tools/7za /tmp/update
+
+    dd if=home_v429m of=verout bs=22 count=1
+    dd if=home_v429m of=home1 bs=22 skip=1 count=999999999999999
+    rm home_v429m
+    dd if=home1 of=enc_key bs=1024 count=1
+    dd if=home1 of=home2 bs=1024 skip=1 count=99999999999999
+    rm home1
+
+    /tmp/update/rsa_pub_dec enc_key dec_enc_key
+    act=$?
+    base=0
+    if [ $act -eq $base ]
+    then
+        echo rsa_pub_dec return pass
+    else
+        echo rsa_pub_dec return fail
+        sync
+        exit
+    fi
+
+    cat dec_enc_key home2 > home3
+    rm home2
+    dd if=home3 of=home4 bs=66 skip=1 count=99999999
+    dd if=home3 of=md5 bs=33 count=1
+    dd if=home3 of=pwd bs=33 skip=1 count=1
+    dd if=home4 of=verin bs=22 count=1
+
+
+    echo "md5:"
+    cat md5
+    echo "pwd:"
+    cat pwd
+    echo "ver_in:"
+    cat verin
+    rm home3
+    dd if=home4 of=home_v429m.7z bs=22 skip=1 count=9999999999999
+    rm home4
+    mv home_v429m.7z home4
+    zpwd=$(cat pwd)
+    ver_out=$(cat verout)
+    ver_in=$(cat verin)
+    if [ -f /home/homever ]; then
+        oldver=$(cat /home/homever)
+    else
+        oldver=0
+    fi
+
+    echo ver_in=$ver_in, ver_out=$ver_out
+    if [ $ver_in != $ver_out ];then
+        echo ver_in!=ver_out, check fail
+        #rm $1
+        sync
+        exit
+    else
+        echo ver_in==ver_out, check pass
+    fi
+
+    echo ver_in=$ver_in, oldver=$oldver
+    if [ $ver_in != $oldver ];then
+        echo ver_in!=oldver, check pass
+    else
+        echo ver_in==oldver, check fail
+        #rm /tmp/sd/home_y30m
+        sync
+        exit
+    fi
+
+    md5_home4=$(md5sum home4 | awk  '{print $1}')
+    md5_in=$(cat md5)
+    if [ $md5_home4 != $md5_in ];then
+        echo md5_home4!=md5_in, check fail
+        sync
+        exit
+    else
+        echo md5_home4==md5_in, check pass
+    fi
+fi
+#./7za x home4 -p$zpwd
+tar -jxvf home4
+/tmp/update/home/app/script/update.sh
+cd -
+echo "------------------------------------home base tools extpkg.sh exit------------------------------------"
