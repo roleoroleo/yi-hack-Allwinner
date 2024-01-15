8c8773f3d6449e110936d14c544ec22e97906ef7
roleo <roleo@iperbole.bologna.it>
2024-01-15 21:09:15 +0100

    Force the cam to work in local time

--- src/www/httpd/cgi-bin/eventsfile.sh
+++ src/www/httpd/cgi-bin/eventsfile.sh
@@ -59,11 +59,8 @@ if [ "$DIR" == "none" ] ; then
 fi
 
 DIRS00="${DIR:0:4}-${DIR:5:2}-${DIR:8:2} ${DIR:11:2}:00"
-#if [ "$HV" == "12" ]; then
-#    DIRS00E=$(date -d "$DIRS00" +"%s")
-#else
-    DIRS00E=$(date -u -d "$DIRS00" +"%s")
-#fi
+#DIRS00E=$(date -u -d "$DIRS00" +"%s")
+DIRS00E=$(date -d "$DIRS00" +"%s")
 DIRL=$(date +%YY%mM%dD%HH -d "@$DIRS00E")
 
 printf "Content-type: application/json\r\n\r\n"
